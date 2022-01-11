4b893e752f7e67b397d0784a4920735e85e64e5b
Trang Nhung <trangnhung080818@gmail.com>
2022-01-11 22:12:46 +0700

    Components part 1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import logo from './logo.svg';
-import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { Component } from 'react';
+import Menu from './components/MenuComponents';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar drak color="primary">
-        <div className='container'>
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar drak color="primary">
+//         <div className='container'>
+//           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+//         </div>
+//       </Navbar>
+      
+//     </div>
+//   );
+// }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Navbar drak color="warning">
+         <div className='container'>
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      
+      <Menu />
     </div>
-  );
+    );
+  }
 }
 
 export default App;
