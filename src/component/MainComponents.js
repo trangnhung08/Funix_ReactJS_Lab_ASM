424b88725b7b42a303a471fa531a46e770f30c8c
Trang Nhung <trangnhung080818@gmail.com>
2022-01-19 23:31:22 +0700

    Exercise 2

--- src/component/MainComponents.js
+++ src/component/MainComponents.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import Home from './HomeComponents';
 import Menu from './MenuComponents';
 import Contact from './ContactComponents';
+import About from './AboutComponents';
 import DishDetail from './DishDetailComponents';
 import Header from './HeaderComponents';
 import Footer from './FooterComponents';
@@ -10,7 +11,6 @@ import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
-
 
 class Main extends Component {
 
@@ -46,13 +46,14 @@ class Main extends Component {
         return (
             <div>
                 <Header />
-                <Switch>
-                    <Route path="/home" component={HomePage} />
-                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
-                    <Route path="/menu/:dishId" component={DishWithId} />
-                    <Route exact path="/contactus" component={Contact} />
-                    <Redirect to="/home" />
-                </Switch>
+                    <Switch>
+                        <Route path="/home" component={HomePage} />
+                        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                        <Route path="/menu/:dishId" component={DishWithId} />
+                        <Route exact path="/contactus" component={Contact} />
+                        <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+                        <Redirect to="/home" />
+                    </Switch>
                 <Footer />
             </div>
         );
